80244c8aef5e74fbfbd1f016089f18db1c4d88f7
Fred Cal <fbcmuinhos@paschoalotto.com.br>
2022-12-09 19:45:02 -0500

    AuthService and signup method created

---
+++ src/users/auth.service.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email already in use');
+    }
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create(email, result);
+
+    return user;
+  }
+
+  // signin(email: string, password: string) {
+
+  // }
+}
