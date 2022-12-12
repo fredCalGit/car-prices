a1d5d9fb5af5cabb1c6bda41edd7d600f608289e
Fred Cal <fbcmuinhos@paschoalotto.com.br>
2022-12-11 20:12:15 -0500

    Estimate feature implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,11 +42,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['1234zxcvasdfqwer'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
